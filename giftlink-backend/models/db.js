b9bbda0e37b91de0484cbd91152ce43eb4553674
cauecavallaro <cauecavallaro@gmail.com>
2024-05-13 15:52:25 -0400

    APIs added to giftRoutes

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -9,20 +9,29 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
+    if (dbInstance) {
         return dbInstance
     };
 
-    const client = new MongoClient(url);      
+    const client = new MongoClient(url);
 
     // Task 1: Connect to MongoDB
     // {{insert code}}
+    
+    let conn;
+    try {
+        conn = await client.connect();
+    } catch (e) {
+        console.error(e);
+    }
 
     // Task 2: Connect to database giftDB and store in variable dbInstance
     //{{insert code}}
+    dbInstance = conn.db(dbName)
 
     // Task 3: Return database instance
     // {{insert code}}
+    return dbInstance
 }
 
 module.exports = connectToDatabase;
