4cc98280dca5d40b1cbb3ec7e61005d3a0fe124e
cauecavallaro <cauecavallaro@gmail.com>
2024-05-15 15:27:08 -0400

    frontend exercise

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -9,29 +9,15 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance) {
+    if (dbInstance){
         return dbInstance
     };
 
     const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
-    
-    let conn;
-    try {
-        conn = await client.connect();
-    } catch (e) {
-        console.error(e);
-    }
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-    dbInstance = conn.db(dbName)
-
-    // Task 3: Return database instance
-    // {{insert code}}
-    return dbInstance
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
