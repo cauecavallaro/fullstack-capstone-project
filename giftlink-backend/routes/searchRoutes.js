e188dbe114b83d335a7187b3e01aca67f7a29261
cauecavallaro <156936474+cauecavallaro@users.noreply.github.com>
2024-05-20 05:55:03 +0100

    Update searchRoutes.js
    Added /*jshint esversion: 8 */
    
    Fixed missing semicolons

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 8 */
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
@@ -20,17 +21,17 @@ router.get('/', async (req, res, next) => {
 
         // Task 3: Add other filters to the query
         if (req.query.category) {
-            query.category = req.query.category
+            query.category = req.query.category;
         }
         if (req.query.condition) {
-            query.condition = req.query.condition
+            query.condition = req.query.condition;
         }
         if (req.query.age_years) {
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
         // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        let gifts = collection.find(query).toArray()
+        let gifts = collection.find(query).toArray();
 
         res.json(gifts);
     } catch (e) {
