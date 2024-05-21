44f9c3eae50da3c9b712688890a0c8c876f29605
cauecavallaro <156936474+cauecavallaro@users.noreply.github.com>
2024-05-21 16:36:07 +0100

    Update LoginPage.js

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,65 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import './LoginPage.css';
-import { urlConfig } from '../../config';
-import { useAppContext } from '../../context/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import React, { useState,useEffect } from 'react';
 
+import './LoginPage.css';
 
 function LoginPage() {
-
-    //insert code here to create useState hook variables for email, password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [incorrect, setIncorrect] = useState('');
-    const navigate = useNavigate();
-    const bearerToken = sessionStorage.getItem('bearer-token');
-    const { setIsLoggedIn } = useAppContext();
-    useEffect(() => {
-        if (sessionStorage.getItem('auth-token')) {
-            navigate('/app')
-        }
-    }, [navigate])
 
-    // insert code here to create handleLogin function and include console.log
     const handleLogin = async (e) => {
         e.preventDefault();
-        //api call
-        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
-            //Step 1 - Task 7
-            method: 'POST',
-            //Step 1 - Task 8
-            headers: {
-                'content-type': 'application/json',
-                'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
-            },
-            //Step 1 - Task 9
-            body: JSON.stringify({
-                email: email,
-                password: password,
-            })
-        });
-        //Step 2: Task 1
-        const json = await res.json();
-        console.log('Json', json);
-        if (json.authtoken) {
-            //Step 2: Task 2
-            sessionStorage.setItem('auth-token', json.authtoken);
-            sessionStorage.setItem('name', json.userName);
-            sessionStorage.setItem('email', json.userEmail);
-            //Step 2: Task 3
-            setIsLoggedIn(true);
-            //Step 2: Task 4
-            navigate('/app');
-        } else {
-            //Step 2: Task 5
-            document.getElementById("email").value = "";
-            document.getElementById("password").value = "";
-            setIncorrect("Wrong password. Try again.");
-            setTimeout(() => {
-                setIncorrect("");
-            }, 2000);
-        }
-    }
+}
 
 
     return (
@@ -76,7 +25,7 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your email"
                                 value={email}
-                                onChange={(e) => { setEmail(e.target.value); setIncorrect("") }}
+                                onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
                         <div className="mb-4">
@@ -87,11 +36,10 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your password"
                                 value={password}
-                                onChange={(e) => { setPassword(e.target.value); setIncorrect("") }}
+                                onChange={(e) => setPassword(e.target.value)}
                             />
-                            {/*Step 2: Task 6*/}
-                            <span style={{ color: 'red', height: '.5cm', display: 'block', fontStyle: 'italic', fontSize: '12px' }}>{incorrect}</span>
                         </div>
+                        {/* Include appropriate error message if login is incorrect*/}
                         <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
                         <p className="mt-4 text-center">
                             New here? <a href="/app/register" className="text-primary">Register Here</a>
